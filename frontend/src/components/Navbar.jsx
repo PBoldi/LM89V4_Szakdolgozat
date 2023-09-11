58fb6787f1f4a04d88cb414e73bf8532654cdfc8
PBoldi <boldi333333@gmail.com>
2023-09-11 08:50:28 +0200

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,26 @@
-import { Fragment, useMemo } from "react";
+import { Fragment, Link, useState } from "react";
 import { useNavigate, useSubmit } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
+import Avatar from "@mui/material/Avatar";
 import Toolbar from "@mui/material/Toolbar";
 import Box from "@mui/material/Box";
+import ListItemText from "@mui/material/ListItemText";
 import LoginIcon from "@mui/icons-material/Login";
 import LogoutIcon from "@mui/icons-material/Logout";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import IconButton from "@mui/material/IconButton";
 
 export default function Navbar({ user }) {
   const navigate = useNavigate();
   const submit = useSubmit();
+
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  function handleCloseUserMenu() {
+    setAnchorElUser(null);
+  }
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -40,17 +50,60 @@ export default function Navbar({ user }) {
                 </IconButton>
               </Fragment>
             ) : (
-              <IconButton
-                color={"inherit"}
-                onClick={() =>
-                  submit(null, { action: "/logout", method: "post" })
-                }
-                size={"small"}
-                sx={{ mr: 2 }}
-              >
-                <LogoutIcon />
-                Kilépés
-              </IconButton>
+              <Fragment>
+                <IconButton
+                  onClick={(event) => setAnchorElUser(event.currentTarget)}
+                  sx={{ p: 0 }}
+                >
+                  <Avatar alt={user?.email} src={user?.profile_picture}>
+                    {user?.email?.[0]}
+                  </Avatar>
+                </IconButton>
+                <Menu
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{ horizontal: "right", vertical: "top" }}
+                  id={"menu-appbar"}
+                  keepMounted
+                  onClick={handleCloseUserMenu}
+                  onClose={handleCloseUserMenu}
+                  open={Boolean(anchorElUser)}
+                  // PaperProps={{ style: { width: 250 } }}
+                  sx={{ mt: "45px" }}
+                  transformOrigin={{ horizontal: "right", vertical: "top" }}
+                >
+                  <MenuItem component={Link} to={"/user-profile"}>
+                    <ListItemText primary={"Profil"} />
+                  </MenuItem>
+                  {user?.trainer_profile ? (
+                    <MenuItem component={Link} to={"/trainer-profile"}>
+                      <ListItemText primary={"Edző profil"} />
+                    </MenuItem>
+                  ) : null}
+                  {user?.athlete_profile ? (
+                    <MenuItem component={Link} to={"/athlete-profile"}>
+                      <ListItemText primary={"Sportolói profil"} />
+                    </MenuItem>
+                  ) : null}
+                  {user?.is_admin ? (
+                    <MenuItem component={Link} to={"/admin"}>
+                      <ListItemText primary={"Admin"} />
+                    </MenuItem>
+                  ) : null}
+                  <MenuItem component={Link} to={"/logout"}>
+                    <IconButton
+                      color={"inherit"}
+                      onClick={() =>
+                        submit(null, { action: "/logout", method: "post" })
+                      }
+                      size={"small"}
+                      sx={{ mr: 2 }}
+                    >
+                      <LogoutIcon />
+                      Kilépés
+                    </IconButton>
+                  </MenuItem>
+                </Menu>
+              </Fragment>
             )}
           </Box>
         </Toolbar>
