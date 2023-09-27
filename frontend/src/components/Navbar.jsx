5000db5f72b4487e8672326d68b1799efe18416d
PBoldi <boldi333333@gmail.com>
2023-09-27 10:52:24 +0200

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,23 +1,34 @@
 import { Fragment, Link, useState } from "react";
 import { useNavigate, useSubmit } from "react-router-dom";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
 import AppBar from "@mui/material/AppBar";
 import Avatar from "@mui/material/Avatar";
-import Toolbar from "@mui/material/Toolbar";
 import Box from "@mui/material/Box";
+import Collapse from "@mui/material/Collapse";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 import IconButton from "@mui/material/IconButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import LoginIcon from "@mui/icons-material/Login";
 import LogoutIcon from "@mui/icons-material/Logout";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
 import SearchIcon from "@mui/icons-material/Search";
+import SportsIcon from "@mui/icons-material/Sports";
+import SportsSoccerIcon from "@mui/icons-material/SportsSoccer";
+import Toolbar from "@mui/material/Toolbar";
 
 export default function Navbar({ user }) {
   const navigate = useNavigate();
   const submit = useSubmit();
 
   const [anchorElUser, setAnchorElUser] = useState(null);
+  const [openAdmin, setOpenAdmin] = useState(false);
 
   function handleCloseUserMenu() {
     setAnchorElUser(null);
@@ -90,27 +101,68 @@ export default function Navbar({ user }) {
                   onClick={handleCloseUserMenu}
                   onClose={handleCloseUserMenu}
                   open={Boolean(anchorElUser)}
-                  // PaperProps={{ style: { width: 250 } }}
+                  PaperProps={{ style: { width: 300 } }}
                   sx={{ mt: "45px" }}
                   transformOrigin={{ horizontal: "right", vertical: "top" }}
                 >
                   <MenuItem component={Link} to={"/user-profile"}>
+                    <ListItemIcon>
+                      <AccountCircleIcon />
+                    </ListItemIcon>
                     <ListItemText primary={"Profil"} />
                   </MenuItem>
                   {user?.trainer_profile ? (
                     <MenuItem component={Link} to={"/trainer-profile"}>
+                      <ListItemIcon>
+                        <SportsIcon />
+                      </ListItemIcon>
                       <ListItemText primary={"Edző profil"} />
                     </MenuItem>
                   ) : null}
                   {user?.athlete_profile ? (
                     <MenuItem component={Link} to={"/athlete-profile"}>
+                      <ListItemIcon>
+                        <FitnessCenterIcon />
+                      </ListItemIcon>
                       <ListItemText primary={"Sportolói profil"} />
                     </MenuItem>
                   ) : null}
                   {user?.is_admin ? (
-                    <MenuItem component={Link} to={"/admin"}>
-                      <ListItemText primary={"Admin"} />
-                    </MenuItem>
+                    <Fragment>
+                      <MenuItem
+                        onClick={(event) => {
+                          event.stopPropagation();
+                          setOpenAdmin(!openAdmin);
+                        }}
+                      >
+                        <ListItemIcon>
+                          <AdminPanelSettingsIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={"Admin"} />
+                        {openAdmin ? <ExpandLess /> : <ExpandMore />}
+                      </MenuItem>
+                      <Collapse in={openAdmin} timeout={"auto"} unmountOnExit>
+                        <MenuItem component={Link} to={"/admin/sports"}>
+                          <ListItemText inset>
+                            <ListItemIcon>
+                              <SportsSoccerIcon />
+                              {"Sportok"}
+                            </ListItemIcon>
+                          </ListItemText>
+                        </MenuItem>
+                        <MenuItem
+                          component={Link}
+                          to={"/admin/person-questions"}
+                        >
+                          <ListItemText inset>
+                            <ListItemIcon>
+                              <QuestionMarkIcon />
+                              {"Kérdések a felhasználókhoz"}
+                            </ListItemIcon>
+                          </ListItemText>
+                        </MenuItem>
+                      </Collapse>
+                    </Fragment>
                   ) : null}
                   <MenuItem component={Link} to={"/logout"}>
                     <IconButton
