0f2153effafb77826c2e33a6610d4692f1186573
PBoldi <boldi333333@gmail.com>
2023-10-25 20:11:11 +0200

    some admin ui/ux

--- frontend/src/routes/protected-admin/PersonQuestions.jsx
+++ frontend/src/routes/protected-admin/PersonQuestions.jsx
@@ -9,6 +9,8 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
+import Paper from "@mui/material/Paper";
+
 import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
 
 export default function PersonQuestions() {
@@ -16,47 +18,59 @@ export default function PersonQuestions() {
 
   return (
     <Fragment>
-      <List sx={{ width: "50%" }}>
+      <List
+        sx={{
+          width: "60%",
+        }}
+      >
         <ListItemButton component={Link} to={"create"}>
           <ListItemIcon>
             <AddIcon />
           </ListItemIcon>
           <ListItemText primary={"Új kérdés felvétele"} />
         </ListItemButton>
-        {personQuestions?.map((personQuestion) => (
-          <ListItem
-            key={personQuestion?.id}
-            secondaryAction={
-              <Fragment>
-                <IconButton
-                  aria-label={"edit"}
-                  component={Link}
-                  edge={"end"}
-                  sx={{ mr: 1 }}
-                  to={`${personQuestion?.id}/edit`}
-                >
-                  <EditIcon />
-                </IconButton>
-                <IconButton
-                  aria-label={"delete"}
-                  component={Link}
-                  edge={"end"}
-                  to={`${personQuestion?.id}/delete`}
-                >
-                  <DeleteIcon />
-                </IconButton>
-              </Fragment>
-            }
-          >
-            <ListItemIcon>
-              <QuestionMarkIcon />
-            </ListItemIcon>
-            <ListItemText
-              primary={personQuestion?.question}
-              secondary={personQuestion?.weight}
-            />
-          </ListItem>
-        ))}
+        <Paper
+          elevation={0}
+          sx={{
+            overflow: "auto",
+            maxHeight: 550,
+          }}
+        >
+          {personQuestions?.map((personQuestion) => (
+            <ListItem
+              key={personQuestion?.id}
+              secondaryAction={
+                <Fragment>
+                  <IconButton
+                    aria-label={"edit"}
+                    component={Link}
+                    edge={"end"}
+                    sx={{ mr: 1 }}
+                    to={`${personQuestion?.id}/edit`}
+                  >
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton
+                    aria-label={"delete"}
+                    component={Link}
+                    edge={"end"}
+                    to={`${personQuestion?.id}/delete`}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </Fragment>
+              }
+            >
+              <ListItemIcon>
+                <QuestionMarkIcon />
+              </ListItemIcon>
+              <ListItemText
+                primary={personQuestion?.question}
+                secondary={personQuestion?.weight}
+              />
+            </ListItem>
+          ))}
+        </Paper>
       </List>
       <Outlet />
     </Fragment>
--- frontend/src/routes/protected-admin/Sports.jsx
+++ frontend/src/routes/protected-admin/Sports.jsx
@@ -9,6 +9,7 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
+import Paper from "@mui/material/Paper";
 import SportsSoccerIcon from "@mui/icons-material/SportsSoccer";
 
 export default function Sports() {
@@ -16,44 +17,52 @@ export default function Sports() {
 
   return (
     <Fragment>
-      <List sx={{ width: "50%" }}>
+      <List sx={{ width: "60%" }}>
         <ListItemButton component={Link} to={"create"}>
           <ListItemIcon>
             <AddIcon />
           </ListItemIcon>
           <ListItemText primary={"Új sport felvétele"} />
         </ListItemButton>
-        {sports?.map((sport) => (
-          <ListItem
-            key={sport?.id}
-            secondaryAction={
-              <Fragment>
-                <IconButton
-                  aria-label={"edit"}
-                  component={Link}
-                  edge={"end"}
-                  sx={{ mr: 1 }}
-                  to={`${sport?.id}/edit`}
-                >
-                  <EditIcon />
-                </IconButton>
-                <IconButton
-                  aria-label={"delete"}
-                  component={Link}
-                  edge={"end"}
-                  to={`${sport?.id}/delete`}
-                >
-                  <DeleteIcon />
-                </IconButton>
-              </Fragment>
-            }
-          >
-            <ListItemIcon>
-              <SportsSoccerIcon />
-            </ListItemIcon>
-            <ListItemText primary={sport?.name} />
-          </ListItem>
-        ))}
+        <Paper
+          elevation={0}
+          sx={{
+            overflow: "auto",
+            maxHeight: 550,
+          }}
+        >
+          {sports?.map((sport) => (
+            <ListItem
+              key={sport?.id}
+              secondaryAction={
+                <Fragment>
+                  <IconButton
+                    aria-label={"edit"}
+                    component={Link}
+                    edge={"end"}
+                    sx={{ mr: 1 }}
+                    to={`${sport?.id}/edit`}
+                  >
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton
+                    aria-label={"delete"}
+                    component={Link}
+                    edge={"end"}
+                    to={`${sport?.id}/delete`}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </Fragment>
+              }
+            >
+              <ListItemIcon>
+                <SportsSoccerIcon />
+              </ListItemIcon>
+              <ListItemText primary={sport?.name} />
+            </ListItem>
+          ))}
+        </Paper>
       </List>
       <Outlet />
     </Fragment>
