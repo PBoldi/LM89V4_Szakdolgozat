f1b13a36012720b9a81b9047361d94b8fd3c79a9
PBoldi <boldi333333@gmail.com>
2023-10-29 09:30:33 +0100

    Update SearchTrainer.jsx

--- frontend/src/routes/protected-authentication/athlete/SearchTrainer.jsx
+++ frontend/src/routes/protected-authentication/athlete/SearchTrainer.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { useLoaderData, useOutletContext, useSubmit } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
