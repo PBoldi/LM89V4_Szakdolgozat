d0a25ed21c5c041bafbeaa1014ad04835b881d16
PBoldi <boldi333333@gmail.com>
2023-11-01 08:57:55 +0100

    Update SearchAthlete.jsx

--- frontend/src/routes/protected-authentication/athlete/SearchAthlete.jsx
+++ frontend/src/routes/protected-authentication/athlete/SearchAthlete.jsx
@@ -154,7 +154,7 @@ export default function SearchTrainer() {
                                 aria-label={"Connect"}
                                 color={"success"}
                                 onClick={() =>
-                                  handleConnection(true, athlete?.id)
+                                  handleConnection(athlete?.id, true)
                                 }
                               >
                                 <DoneOutlineIcon />
@@ -166,7 +166,7 @@ export default function SearchTrainer() {
                                 aria-label={"Don't connect"}
                                 color={"error"}
                                 onClick={() =>
-                                  handleConnection(false, athlete?.id)
+                                  handleConnection(athlete?.id, false)
                                 }
                               >
                                 <CloseIcon />
