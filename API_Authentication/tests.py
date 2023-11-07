1d7f26c222c76145bacc354a605224b9fdaa5e45
PBoldi <boldi333333@gmail.com>
2023-11-07 12:08:30 +0100

    athlete and trainer profile tests

--- API_Authentication/tests.py
+++ API_Authentication/tests.py
@@ -18,3 +18,74 @@ class AuthenticatedUserTestCase(APITestCase):
         )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['email'], "test@test.test")
+
+
+class AthleteProfileTestCase(APITestCase):
+    def setUp(self):
+        data = {"email":"test@test.test", "password":"test"}
+        response = self.client.post('http://localhost:8000/auth/users/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        response = self.client.post('http://localhost:8000/auth/token/', data, format='json')
+        AthleteProfileTestCase.access = json.loads(response.content)['access']
+
+        response = self.client.get(
+            'http://localhost:8000/auth/users/authenticated/', HTTP_AUTHORIZATION='Bearer {}'.format(AthleteProfileTestCase.access), format='json'
+        )
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['email'], "test@test.test")
+
+        data = {"user": 1, "biography": "Test biography"}
+        response = self.client.post('http://localhost:8000/auth/athlete-profile/', data, HTTP_AUTHORIZATION='Bearer {}'.format(AthleteProfileTestCase.access), format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_athlete_profile(self):
+        response = self.client.get(f'http://localhost:8000/auth/athlete-profile/{1}', HTTP_AUTHORIZATION='Bearer {}'.format(AthleteProfileTestCase.access), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["biography"], "Test biography")
+    
+    def test_update_athlete_profile(self):
+        data = { "biography": "Test biography!"}
+        response = self.client.patch(f'http://localhost:8000/auth/athlete-profile/{1}', data, HTTP_AUTHORIZATION='Bearer {}'.format(AthleteProfileTestCase.access), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["biography"], "Test biography!")
+       
+
+class TrainerProfileTestCase(APITestCase):
+    def setUp(self):
+        data = {"email":"test@test.test", "password":"test"}
+        response = self.client.post('http://localhost:8000/auth/users/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        response = self.client.post('http://localhost:8000/auth/token/', data, format='json')
+        AthleteProfileTestCase.access = json.loads(response.content)['access']
+
+        response = self.client.get(
+            'http://localhost:8000/auth/users/authenticated/', HTTP_AUTHORIZATION='Bearer {}'.format(AthleteProfileTestCase.access), format='json'
+        )
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['email'], "test@test.test")
+
+        data = {"user": 1, "biography": "Test biography", "is_available_online": True, "is_dietician": True, "price_per_hour": 50}
+        response = self.client.post('http://localhost:8000/auth/trainer-profile/', data, HTTP_AUTHORIZATION='Bearer {}'.format(AthleteProfileTestCase.access), format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_trainer_profile(self):
+        response = self.client.get(f'http://localhost:8000/auth/trainer-profile/{1}', HTTP_AUTHORIZATION='Bearer {}'.format(AthleteProfileTestCase.access), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["biography"], "Test biography")
+        self.assertEqual(response.data["is_available_online"], True)       
+        self.assertEqual(response.data["is_dietician"], True)       
+        self.assertEqual(response.data["price_per_hour"], 50)     
+    
+    def test_update_trainer_profile(self):
+        data = {"biography": "Test biography!", "is_available_online": False, "is_dietician": False, "price_per_hour": 55}
+        response = self.client.patch(f'http://localhost:8000/auth/trainer-profile/{1}', data, HTTP_AUTHORIZATION='Bearer {}'.format(AthleteProfileTestCase.access), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["biography"], "Test biography!")       
+        self.assertEqual(response.data["is_available_online"], False)       
+        self.assertEqual(response.data["is_dietician"], False)       
+        self.assertEqual(response.data["price_per_hour"], 55)       
+
