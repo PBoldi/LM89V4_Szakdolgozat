b7499ffc3d145438575cea9dea7c8b7b67876a0a
PBoldi <boldi333333@gmail.com>
2023-11-07 11:35:18 +0100

    first test

--- API_Authentication/tests.py
+++ API_Authentication/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+import json
 
-# Create your tests here.
+
+class AuthenticatedUserTestCase(APITestCase):
+    def setUp(self):
+        data = {"email":"test@test.test", "password":"test"}
+        response = self.client.post('http://localhost:8000/auth/users/', data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.client.post('http://localhost:8000/auth/token/', data, format='json')
+        AuthenticatedUserTestCase.access_token = json.loads(response.content)['access']
+
+    def test_authenticated_user(self):
+        response = self.client.get(
+            'http://localhost:8000/auth/users/authenticated/', HTTP_AUTHORIZATION='Bearer {}'.format(AuthenticatedUserTestCase.access_token), format='json'
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['email'], "test@test.test")
