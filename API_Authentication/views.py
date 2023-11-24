83d48223a57f3f2946d59789ebcae5e17ef6357a
PBoldi <boldi333333@gmail.com>
2023-11-24 10:50:56 +0100

    Update views.py

--- API_Authentication/views.py
+++ API_Authentication/views.py
@@ -20,6 +20,7 @@ def standardize(column):
     new_column = (column - column.mean()) / (column.max() - column.min())
     return new_column
 
+
 def get_user_similarities(athlete_profiles_df):
     person_question_weighing_df = pd.DataFrame.from_records(
             PersonQuestionWeighing.objects.all().values( 'athlete_profile__id', 'weight', 'person_question__question'))
@@ -56,7 +57,7 @@ def get_recommended_users(user):
             connected_user_similarity_score = connected_similarity_df[athlete_profile_id]
             connected_user_similarity_score_ordered = connected_user_similarity_score.sort_values(ascending=False)
 
-            random_connected_user = random.randint(1, len(connected_user_similarity_score_ordered)-1)
+            random_connected_user = random.randint(1, len(connected_user_similarity_score_ordered)-1 if len(connected_user_similarity_score_ordered) < 11 else 10)
             athlete_profile_id = connected_user_similarity_score_ordered.index.values.tolist()[random_connected_user]
 
         athlete_profiles_df = read_frame(AthleteProfile.objects.all())
